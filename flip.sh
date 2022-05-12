4de1d6b5ac3f36a03e7dba3dceeaaada7d2876cc
asjadazmi <Asjadazmi0786@gmail.com>
2022-05-12 09:01:26 +0530

    displaying whether the coin flip is head or tail

--- flip.sh
+++ flip.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 echo "welcome to the flip coin problem"
+random=$((RANDOM%2))
+ishead=1;
+
+if [ $ishead -eq $random ]
+then 
+    echo "this is the head";
+else
+    echo "this is tail";
+fi
